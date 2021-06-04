b2660162fda98be5ab6ef43487a6bcca542fa9d1
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-06-04 13:47:21 -0700

    connecting to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose= require("mongoose");
 
-
+const PORT=process.env.PORT||3000;
 const db = require ("./models");
 
 const app = express();
@@ -21,6 +21,6 @@ app.use(require("./routes/html"));
 
     
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("App running on port 3000!");
 });
